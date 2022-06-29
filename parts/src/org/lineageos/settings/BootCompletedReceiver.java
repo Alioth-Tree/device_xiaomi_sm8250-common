3f8a414cc9b31cdf125f5c5ae5718e64dca04bae
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-06-29 15:40:24 +0530

    sm8250-common: parts: Implement Pocket Detection service
    This is a basic pocket detection service that uses
    Xiaomi's custom "large area touch sensor" to prevent
    accidentally triggering lockscreen controls while
    the device is in pocket. We cannot use traditional
    proximity + light sensor based pocket detection, as
    we have ultrasound proximity sensor.
    
    Inspired by the same usage in stock MIUI:
    https://gist.github.com/ghostrider-reborn/4c7f24151db7639a0244caa87178e521
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -28,6 +28,7 @@ import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.doze.PocketService;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.utils.FileUtils;
@@ -43,6 +44,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) return;
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         try {
@@ -57,5 +59,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
         FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
+
+        // Pocket
+        PocketService.startService(context);
+
     }
 }
