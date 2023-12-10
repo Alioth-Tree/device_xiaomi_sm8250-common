73db7115dc76d3ae2124f3feb1f2c38ebc4616f2
Ghosuto <clash.raja10@gmail.com>
2023-12-10 09:14:06 +0100

    sm8250-common: Switch viperFX package repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/DanipunK1/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+git clone https://github.com/Alioth-Tree/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
